89447112e5d8e809709dc59b8104226cd086a0cd
KatelynSITH <158211680+KatelynSITH@users.noreply.github.com>
2024-02-14 13:58:35 -0500

    y

--- gcf.py
+++ gcf.py
@@ -1,15 +1,7 @@
-x = (int(input("Please type the number for the gcf ")))
-y = (int(input("Please type the second number for the gcf ")))
+a = (int(input("Please type the number for the gcf ")))
+b = (int(input("Please type the second number for the gcf ")))
 
-def gcf(x): 
-
-    for i in range(1, x + 1):
-        if x % i == 0:
-            print(i)
-  
-values = gcf(x)
-print('Factoring next #')
-values1 = gcf(y)
-
-GCF_list = [values, values1] 
-print(GCF_list)
+i = 1
+while(i <= a and i <= b):
+    if(a % i == 0 and b % i == 0):
+        print("GCD of {a} and {b} is {gcd(a, b)}")
