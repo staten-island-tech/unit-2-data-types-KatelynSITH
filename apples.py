d7d071b90cb93ae1c387996c48f422b2f65cfcd7
KatelynSITH <158211680+KatelynSITH@users.noreply.github.com>
2024-02-15 14:09:54 -0500

    ty

---
+++ apples.py
@@ -0,0 +1,9 @@
+H = int(input('amount of health points '))
+A = int(input('amount of apples he will eat '))
+S = int(input('amount of seconds he waits '))
+
+def apples(H, A, S):
+    for i in range(1, S+1):
+        1 <= H, A, S <= 10^5
+        print(apples)
+
